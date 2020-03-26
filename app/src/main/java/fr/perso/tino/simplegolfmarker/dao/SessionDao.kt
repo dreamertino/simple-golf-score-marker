0c96ee9b7f0cd2812bb53a5fc74374b5c91d6fae
tino <dreamertino@gmail.com>
2020-03-26 08:30:28 +0100

    feat: Display history

--- app/src/main/java/fr/perso/tino/simplegolfmarker/dao/SessionDao.kt
+++ app/src/main/java/fr/perso/tino/simplegolfmarker/dao/SessionDao.kt
@@ -1,14 +1,20 @@
 package fr.perso.tino.simplegolfmarker.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import fr.perso.tino.simplegolfmarker.model.HoleResult
 import fr.perso.tino.simplegolfmarker.model.SessionResult
 
 @Dao
 interface SessionDao {
+    @Query("SELECT * FROM SessionResult")
+    fun getAllSessions(): LiveData<List<SessionResult>>
 
+    @Query("SELECT * FROM HoleResult")
+    fun getHRForSession(sessionId: Long): LiveData<List<HoleResult>>
 
     @Insert
     fun insertAll(vararg courses: SessionResult)
